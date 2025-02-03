a1b174eed9c3278cda183d45011c0f7ed901f3a4
FaizanGT <faizan.darwesh@genetechsolutions.com>
2025-02-03 12:25:53 +0500

    Add express app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import dotenv from 'dotenv'
-import databaseConnection from "./database/connection.js"
+import dotenv from "dotenv";
+import app from "./app.js"
+import databaseConnection from "./database/connection.js";
 
 dotenv.config({
-path : './env'
-})
+  path: "./env",
+});
 
 databaseConnection()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=> {
+        console.log("App is listening to port",process.env.PORT)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB Connection Failed",err)
+})
