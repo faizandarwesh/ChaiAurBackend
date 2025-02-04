fac6676280822f231c4cf1c377a968bbe2d54cc2
FaizanGT <faizan.darwesh@genetechsolutions.com>
2025-02-04 12:41:30 +0500

    Add pre hook in user model for password encryption using bcrypt and add refresh and access token methods

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from mongooseAggregatePaginate
 
 const videoSchema = new mongoose.Schema(
   {
@@ -36,5 +37,7 @@ const videoSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
